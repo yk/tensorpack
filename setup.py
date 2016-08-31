f08c1b271a7400ba7a0a8894a65e6225b3351e0d
Yannic Kilcher <yannic.kilcher@gmail.com>
2016-08-31 16:55:05 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tensorpack',
+        version='0.0.1',
+        description='blabla',
+        url='http://github.com/yk/tensorpack',
+        author='yk',
+        license='MIT',
+        packages=['tensorpack'],
+        zip_safe=False)
