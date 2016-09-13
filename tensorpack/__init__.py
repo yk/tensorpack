b8917a0a9b02b3216c70097f3507e09d34d37794
Yannic Kilcher <yannic.kilcher@gmail.com>
2016-09-13 10:49:13 +0200

    init

--- tensorpack/__init__.py
+++ tensorpack/__init__.py
@@ -5,13 +5,6 @@
 import numpy # avoid https://github.com/tensorflow/tensorflow/issues/2034
 import cv2  # avoid https://github.com/tensorflow/tensorflow/issues/1924
 
-import models
-import train
-import utils
-import tfutils
-import callbacks
-import dataflow
-
 from .train import *
 from .models import *
 from .utils import *
@@ -19,6 +12,8 @@ from .tfutils import *
 from .callbacks import *
 from .dataflow import *
 from .predict import *
+
+__all__ = ["models", "train", "utils", "tfutils", "callbacks", "dataflow"]
 
 if int(numpy.__version__.split('.')[1]) < 9:
     logger.warn("Numpy < 1.9 could be extremely slow on some tasks.")
